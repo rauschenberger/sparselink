b82291582cf8a92d8b50293248af6feb2bb92fa3
Armin Rauschenberger <Armin.Rauschenberger@lih.lu>
2025-05-30 10:06:52 +0200

    update docs

--- package-development.R
+++ package-development.R
@@ -2,7 +2,7 @@
 # --- code for package development ---
 
 rm(list=ls(all.names=TRUE))
-install.packages(c("roxygen2","pkgdown","rcmdcheck","usethis","remotes","testthat","devtools"))
+#install.packages(c("roxygen2","pkgdown","rcmdcheck","usethis","remotes","testthat","devtools"))
 setwd("C:/Users/arauschenberger/Desktop/sparselink/package")
 # usethis::use_mit_license()
 roxygen2::roxygenise()
