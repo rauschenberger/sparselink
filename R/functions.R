3acc5bd4690bcb8837b3b8b4d6e56e000f66caab
Armin Rauschenberger <Armin.Rauschenberger@lih.lu>
2025-05-30 09:30:11 +0200

    move pkg devel code

--- R/functions.R
+++ R/functions.R
@@ -1,18 +1,4 @@
 
-if(FALSE){
-  #--- code for package development ---
-  #rm(list=ls(all.names=TRUE))
-  #install.packages(c("roxygen2","pkgdown","rcmdcheck","usethis","remotes","testthat","devtools"))
-  #setwd("C:/Users/arauschenberger/Desktop/sparselink/package")
-  # usethis::use_mit_license()
-  #roxygen2::roxygenise()
-  #rcmdcheck::rcmdcheck()
-  #pkgdown::check_pkgdown()
-  #devtools::build()
-  #devtools::submit_cran()
-  #pkgdown::build_site()
-}
-
 #----- main functions -----
 
 #'@title
