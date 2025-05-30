4f4a1615546bd48e0ca660b3eb2c31887fe08d3f
Armin Rauschenberger <Armin.Rauschenberger@lih.lu>
2025-05-30 09:07:46 +0200

    deactivate devel code

--- R/functions.R
+++ R/functions.R
@@ -1,14 +1,15 @@
 
 if(FALSE){
-  rm(list=ls(all.names=TRUE))
+  #--- code for package development ---
+  #rm(list=ls(all.names=TRUE))
   #install.packages(c("roxygen2","pkgdown","rcmdcheck","usethis","remotes","testthat","devtools"))
-  setwd("C:/Users/arauschenberger/Desktop/sparselink/package")
-  roxygen2::roxygenise()
+  #setwd("C:/Users/arauschenberger/Desktop/sparselink/package")
   # usethis::use_mit_license()
-  rcmdcheck::rcmdcheck()
-  pkgdown::check_pkgdown()
-  devtools::build()
-  devtools::submit_cran()
+  #roxygen2::roxygenise()
+  #rcmdcheck::rcmdcheck()
+  #pkgdown::check_pkgdown()
+  #devtools::build()
+  #devtools::submit_cran()
   #pkgdown::build_site()
 }
 
